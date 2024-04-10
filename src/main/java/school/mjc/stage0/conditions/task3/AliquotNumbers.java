34a511bf12af06bf9997124ca1d2084bd68e7b87
Tursyn Sarmassa <tursyn.sarmassa@gmail.com>
2024-04-10 03:24:23 -0400

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        if (second == 0) {
+            System.out.println("Cannot divide by zero");
+        } else if (second % first == 0) {
+            System.out.println("Aliquot");
+        } else {
+            System.out.println("Not aliquot");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        } else {
+            double taxRate;
+            if (salary <= 10000) {
+                taxRate = 0.15;
+            } else if (salary <= 20000) {
+                taxRate = 0.18;
+            } else {
+                taxRate = 0.20;
+            }
+            double salaryAfterTaxes = salary * (1 - taxRate);
+            System.out.println((double)Math.round(salaryAfterTaxes));
+        }
     }
 }
