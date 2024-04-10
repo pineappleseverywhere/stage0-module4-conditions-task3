e7aa9dada254fd548f778163251ec695f990c878
Tursyn Sarmassa <tursyn.sarmassa@gmail.com>
2024-04-10 03:33:17 -0400

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        char lowerChar = Character.toLowerCase(character);
+        if (lowerChar < 'a' || lowerChar > 'z') {
+            System.out.println("wrong alphabet!");
+        } else if ("aeiou".indexOf(lowerChar) != -1) {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (month >= 1 && month <= 12) {
+            System.out.println(daysInMonth[month - 1]);
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
